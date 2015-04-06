4cd3facdf4e9472f1ceb1218148a147b64d6d02e
Michiel van Liempt <michiel.v.liempt@gmail.com>
2015-04-06 19:38:29 +0200

    make-android copies template to current dir

--- src/android/index.js
+++ src/android/index.js
@@ -1,62 +1,59 @@
 var fs = require('vigour-fs')
   , ncp = require('ncp')
-  , path = path('path')
+  , path = require('path')
+  , Promise = require('promise')
+  
+  , _mkdir = Promise.denodeify(fs.mkdirp)
+  , _ncp = function() {
+    var args = Array.prototype.slice.call(arguments)
+    return new Promise(function(resolve, reject) {
+      try {
+        console.log("args", args)
+        args.push(function(err) {
+          if (err) {
+            reject(err)
+          } else {
+            resolve()
+          }
+        })
+        console.log('ncp', args)
+        ncp.apply(this, args)
+        console.log("WHA")
+      } catch (e) {
+        console.log("ERRRRR", e, e.stack)
+        reject(e)
+      }
+    })}
 
-exports = function() {
-  console.log('start android build')
-
-  if(!fs.existsSync('build/android')) {
+function installTemplate() {
+  // if(!fs.existsSync('build/android')) {
     console.log('- Copying android template')
-    ncp(path.join(__dirname, '../..', 'templates/android'), path.join(process.cwd(), ''
-  }
+    var dst = path.join(process.cwd(), 'build/android')
+      , src = path.join(__dirname, '..', '..', 'templates', 'android')
 
-
-
-
-  console.log('end android build')
+    console.log(typeof src, typeof dst)
+    return _mkdir(dst)
+           .then(_ncp( src
+                     , dst
+                     , {clobber: true}))
+  // }
+  // return new Promise(function(resolve, reject) {
+    // resolve('huppeldepup')
+  // })
 }
 
-function exists(path) {
-  fs.exists
+function compile() {
+  console.log("we should be compiling now")
+  return true;
 }
 
-function cp(source, target, cb) {
-  var cbCalled = false
-    , rs
-    , ws
-
-  targetDir = target.slice(0, target.lastIndexOf('/'))
-  fs.mkdirp(targetDir, function (err) {
-  var cbCalled = false
-    , rs
-    , ws
-  if (err) {
-    cb(err)
-    return
-  }
-    rs = fs.createReadStream(source)
-    ws = fs.createWriteStream(target)
-
-  log.info('Copying', source, 'to', target)
-  rs.on("error", function (err) {
-    done(err)
-  })
-
-  ws.on("error", function (err) {
-    done(err)
-  })
-
-  ws.on("close", function (ex) {
-    done(null)
-  })
-
-  rs.pipe(ws)
-
-  function done (err) {
-    if (!cbCalled) {
-      cb(err)
-      cbCalled = true
-    }
-  }
-  })
+module.exports = exports = function() {
+  console.log('start android build')
+  installTemplate()
+    .then(compile)
+    .then(console.log('end android build'))
+    .catch(function(reason) {
+        console.error(reason)
+    })
 }
+
--- src/index.js
+++ src/index.js
@@ -1,21 +1,3 @@
-//here we need some lazy mechanism 
-exports.util = require('./util')
-exports.test = exports.util.isNode && require('./test')
-
-Object.defineProperty(exports, 'mocha', {
-	get:function() {
-		return require('./test/mocha')
-	}
-})
-
-if( !exports.util.isNode ) {
-	//make the interfaces to a server
-	var server = require('./server/client')
-	//dit is de client side interface!
-	exports.api = server.request
-
-} 
-
 exports.build = {
   android: require('./android')
 }
