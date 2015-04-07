77e1e16b00393061452eb8038d98c2ebe0a28409
Michiel van Liempt <michiel.v.liempt@gmail.com>
2015-04-07 11:54:32 +0200

    android building

--- src/android/index.js
+++ src/android/index.js
@@ -2,31 +2,82 @@ var fs = require('vigour-fs')
   , ncp = require('ncp')
   , path = require('path')
   , Promise = require('promise')
-  
+  , proc = require('child_process')
+  , exec = proc.exec
+
+  // vars
+  , workingDir = process.cwd()
+  , localBuildDir = path.join(workingDir, 'build', 'android')
+  , androidOutputDir = path.join(localBuildDir, 'app', 'build', 'outputs', 'apk')
+
+  // fns that return promises
   , _mkdir = Promise.denodeify(fs.mkdirp)
   , _ncp = Promise.denodeify(ncp)
+  , _exe = makePromising(exe)
+
+function exe (command, cwd, cb) {
+  log.info('Executing', command)
+  if (cwd === '') {
+    cwd = process.cwd()
+  }
+  log.info('in', cwd)
+  exec( command
+      , { cwd: cwd
+        , maxBuffer: 1024 * 500
+        }
+      , function (error, stdout, stderr) {
+          if (error) {
+            log.error('Error executing ' + command, error)
+            return
+          }
+          // log.info('stdout', stdout)
+          log.error('stderr', stderr)
+          // log.info('command succeeded')
+          cb()
+        })
+}
+
+function makePromising(fn) {
+  return function() {
+    var args = Array.prototype.slice.call(arguments)
+    function cb(err) {
+      if (err) {
+        reject(err)
+        return
+      }
+      resolve()
+    }
+    args.push(cb)
+    return new Promise(function (resolve, reject) {
+                         Function.prototype.apply(fn, args)
+                       })
+  }
+}
 
 function installTemplate() {
   // if(!fs.existsSync('build/android')) {
     console.log('- Copying android template')
     var src = path.join(__dirname, '..', '..', 'templates', 'android')
-      , dst = path.join(process.cwd(), 'build/android')
-
+      
     console.log("from", src)
-    console.log("to", dst)
-    return _mkdir(dst)
+    console.log("to", workingDir)
+    return _mkdir(workingDir)
       .then(function () {
         _ncp(src
-          , dst
+          , workingDir
           , {clobber: true})
       })
       .catch(function (reason) {
-        console.error("Oops", reason)
+        console.error("couldn't copy template: ", reason)
       })
 }
 
 function compile() {
   console.log("Compiling (coming soon)")
+  var packageName = 'io.vigour.cloudandroidwrapper'
+  _exe('./gradlew assembleDebug', localBuildDir)
+    .then(_exe('adb install -r ' + packageName, androidOutputDir))
+    .then(_exe('adb shell monkey -p ' + packageName + ' 1', ''))
   return true;
 }
 
