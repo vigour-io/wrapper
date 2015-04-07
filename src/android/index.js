1f1f7fd8aa4747204fbc4c5f35ff011f1721c71a
Michiel van Liempt <michiel.v.liempt@gmail.com>
2015-04-07 14:01:10 +0200

    building android works

--- src/android/index.js
+++ src/android/index.js
@@ -4,36 +4,46 @@ var fs = require('vigour-fs')
   , Promise = require('promise')
   , proc = require('child_process')
   , log = require('npmlog')
-  , exec = proc.exec
+  , spawn = proc.spawn
 
   // vars
   , workingDir = process.cwd()
   , localBuildDir = path.join(workingDir, 'build', 'android')
   , androidOutputDir = path.join(localBuildDir, 'app', 'build', 'outputs', 'apk')
+  , packageName = 'io.vigour.cloudandroidwrapper'
 
   // fns that return promises
   , _mkdir = Promise.denodeify(fs.mkdirp)
   , _ncp = Promise.denodeify(ncp)
 
 function exe (command, cwd) {
+  var args = command.split(' ')
+  var fnName = args[0]
+  args = args.splice(1, args.length-1)
   return new Promise(function(fulfill, reject) {
     log.info('Executing', command)
     if (cwd === '') {
       cwd = process.cwd()
     }
     log.info('in', cwd)
-    exec( command
-        , { cwd: cwd
-          , maxBuffer: 1024 * 500
-          }
-        , function (error, stdout, stderr) {
-            if (error) {
-              log.error('Error executing ' + command, error)
-              reject(error)
-              return
-            }
-            fulfill()
-          })
+    var call = spawn( fnName , args , { cwd: cwd } )
+    log.info('created call', fnName, args)
+    call.stdout.on('data', function (data) {
+      process.stdout.write(data)
+    });
+
+    call.stderr.on('data', function (data) {
+      process.stderr.write(data)
+    });
+
+    call.on('close', function(code) {
+                        log.info('call done', command, code)
+                        if (code === 0) {
+                          fulfill()
+                        } else {
+                          reject()
+                        }
+                      })
   })
 }
 
@@ -43,11 +53,11 @@ function installTemplate() {
     var src = path.join(__dirname, '..', '..', 'templates', 'android')
       
     console.log("from", src)
-    console.log("to", workingDir)
-    return _mkdir(workingDir)
+    console.log("to", localBuildDir)
+    return _mkdir(localBuildDir)
       .then(function () {
         _ncp(src
-          , workingDir
+          , localBuildDir
           , {clobber: true})
       })
       .catch(function (reason) {
@@ -60,18 +70,20 @@ function installTemplate() {
 /////////////////////////////////////////////
 
 function assembleDebug() {
+  log.info("start assembleDebug")
   return exe('./gradlew assembleDebug', localBuildDir) 
 }
 function install() {
+  log.info("start install")
   return exe('adb install -r ' + packageName, androidOutputDir) 
 }
 function run() {
+  log.info("start run")
   return exe('adb shell monkey -p ' + packageName + ' 1', '')
 }
 
 function compile() {
   console.log("Compiling")
-  var packageName = 'io.vigour.cloudandroidwrapper'
   return assembleDebug()
            .then(install)
            .then(run)
