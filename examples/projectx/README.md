e1585a259133af415355abaed7069c1f2d786a7e
Shawn Inder <shawn@vigour.io>
2015-04-09 15:02:36 +0200

    fixes

--- examples/projectx/README.md
+++ examples/projectx/README.md
@@ -7,6 +7,10 @@ projectx
 
 ##Build
 
+`npm start` builds the web app and watches the files to rebuild when they change
+
+##Build native
+
 - `npm run build -- [platforms...]`
 - `npm run build`
 - `npm run build -- android`
--- examples/projectx/www/index.js
+++ examples/projectx/www/index.js
@@ -1,25 +1,131 @@
 
 var bridge = require('../../../lib/bridge')
-document.getElementById('thebutton').addEventListener("click", start)
+  , Element = require('vigour-js/ui/element')
+
+var app = new Element({
+  node: document.body
+  , header:
+  {
+    node: 'h1'
+    , text: "Testing the native bridge"
+  }
+  , t1:
+  {
+    node: 'button'
+    , text: "send 'test message'"
+    , events:
+    {
+      click: function () {
+        testOneWay('test message')
+      }
+    }
+  }
+  , t2:
+  {
+    node: 'button'
+    , text: "dummy/dummy()"
+    , events:
+    {
+      click: function () {
+        testSimple()
+      }
+    }
+  }
+  , t3:
+    {
+      node: 'button'
+      , text: "statusbar/hide()"
+      , events:
+      {
+         click: function () {
+          testSimple('statusbar','hide')
+        }
+      }
+    }
+    , t4:
+    {
+      node: 'button'
+      , text: "statusbar/show()"
+      , events:
+      {
+         click: function () {
+          testSimple('statusbar','show')
+        }
+      }
+    }
+    , t5:
+    {
+      node: 'button'
+      , text: "test/log('test log message')"
+      , events:
+      {
+         click: function () {
+          testSimple('test', 'log', 'test log message')
+        }
+      }
+    }
+    , t6:
+    {
+      node: 'button'
+      , text: "test/echo('test echo message')"
+      , events:
+      {
+         click: function () {
+          testSimple('test', 'echo', 'test echo message')
+        }
+      }
+    }
+    , t7:
+    {
+      node: 'button'
+      , text: "test/vibrate()"
+      , events:
+      {
+         click: function () {
+          testSimple('test', 'vibrate')
+        }
+      }
+    }
+    , t8:
+    {
+      node: 'button'
+      , text: "test/getTime()"
+      , events:
+      {
+         click: function () {
+          testSimple('test', 'getTime')
+        }
+      }
+    }
+    , container:
+    {
+      attr: { id: "container" }
+    }
+})
+
 
 function testSend (msg) {
   bridge.send(msg)
 }
-function testOneWay(message) {
+function testOneWay (message) {
     wrapTry(testSend)(message)
 }
 
-function testSimple() {
-  wrapTry(callNative)('dummy', 'dummy', [], function(error, value) {
-    if (error) {
-      bridge.addToDom("error: " + error)
-    } else {
-      bridge.addToDom("success!: " + value)
-    }
-  })
+function testSimple () {
+    var plugin = arguments[0] || 'dummy'
+    var fnName = arguments[1] || 'dummy'
+    var args = arguments[2] || []
+    wrapTry(callNative)(plugin, fnName, args, function(error, value) {
+        if (error) {
+            addToDom("error: " + error)
+        } else {
+            addToDom("success!: " + value)
+        }
+    })
+
 }
 
-function wrapTry(fn) {
+function wrapTry (fn) {
   return function() {
     try{
       fn.apply(fn, arguments)
