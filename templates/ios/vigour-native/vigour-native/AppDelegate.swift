afe5de95cb8243b69f0d67e5675e8f62514b38df
Alexander van der Werff <avdwerff@gmail.com>
2015-04-09 11:46:02 +0200

    native bridge handler

--- templates/ios/vigour-native/vigour-native/AppDelegate.swift
+++ templates/ios/vigour-native/vigour-native/AppDelegate.swift
@@ -6,7 +6,21 @@
 //  Copyright (c) 2015 Vigour.io. All rights reserved.
 //
 
+// WKWebView has an issue not able load html files from local disk
+// http://openradar.appspot.com/20160687
+// https://issues.apache.org/jira/browse/CB-7539
+// https://github.com/shazron/WKWebViewFIleUrlTest
+
+// two options to bypass the issue:
+//  - user a server like: https://github.com/swisspol/GCDWebServer, https://github.com/robbiehanson/CocoaHTTPServer
+//    or https://developer.apple.com/legacy/library/samplecode/CocoaHTTPServer/Listings/CocoaHTTPServer_m.html
+// - workaround is to copy www folder to tmp folder in de apps sandbox
+
 import UIKit
+
+
+let webApplicationRootFolderName = "www"
+let webAplicationFolderPath = NSTemporaryDirectory().stringByAppendingPathComponent(webApplicationRootFolderName)
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,9 +29,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     lazy var mainController: MainController = {
         return MainController()
     }()
-
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 
+        copyWWWToTmpFolderIfNeeded(true)
+        
         self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
         
         self.window?.rootViewController = mainController.mainViewController
@@ -27,5 +43,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
+    func applicationWillEnterForeground(application: UIApplication) {
+        copyWWWToTmpFolderIfNeeded(false)
+    }
+    
+    func copyWWWToTmpFolderIfNeeded(force: Bool) {
+        
+        let filemgr = NSFileManager.defaultManager()
+        
+        var error: NSError?
+        if filemgr.fileExistsAtPath(webAplicationFolderPath) && !force {
+            if let e = error {
+                println("\(e.localizedDescription)")
+                return
+            }
+            return
+        }
+
+        //wkwebview fix for referencing to www folder
+        copyFolderToFolder(NSBundle.mainBundle().pathForResource("www", ofType: nil)!, webAplicationFolderPath)
+    }
+    
 }
 
