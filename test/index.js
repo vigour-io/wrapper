62bf36f29f7dd2e193250852f423add1ace841d4
Shawn Inder <shawn@vigour.io>
2015-04-10 23:57:10 +0200

    fixes!

---
+++ test/index.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+var npm = require('npm')
+  , log = require('npmlog')
+  , errors = 0
+  , warnings = 0
+
+npm.load(function (err, npm) {
+  if (err) {
+    log.error(err)
+    errors += 1
+  } else {
+    if (npm.config.get('user-agent')
+        .split(' ')[0]
+        .split('/')[1][0] !== "2") {
+      log.warn('sudo npm update npm@^2 -g')
+      warnings += 1
+    }
+  }
+  if (errors) {
+    log.error(errors, "errors")
+  }
+  if (warnings) {
+    log.warn(warnings, "warnings")
+  }
+  if (!errors & !warnings) {
+    log.info("PASS")
+  }
+})
