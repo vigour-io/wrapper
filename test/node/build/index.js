facdbd0d66baa5719609d18bf0f048480870924e
Shawn Inder <shawn@vigour.io>
2015-08-25 10:48:17 +0200

    lint

--- test/node/build/index.js
+++ test/node/build/index.js
@@ -1,45 +1,47 @@
+/* global describe, it, expect */
+
 var path = require('path')
 var build = require('../../../lib/build')
 
 // TODO Remove dependency on vigour-example being checkout-out in same directory as vigour-native, perhaps by making vigour-example a submodule?
 var repo = path.join(__dirname
-	, '..', '..', '..', '..', 'vigour-example')
+  , '..', '..', '..', '..', 'vigour-example')
 var pkgPath = path.join(repo, 'package.json')
 var opts =
-	{ configFiles: pkgPath
-	, vigour:
-		{ native:
-			{
-				root: repo
-			}
-		}
-	}
+  { configFiles: pkgPath,
+  vigour:
+    { native:
+      {
+        root: repo
+      }
+    }
+  }
 
 var options = JSON.stringify(opts)
 var timeout = 20000
 
 describe("build", function () {
-	it("ios should succeed in under " + timeout +  " milliseconds!"
-	, function () {
-		this.timeout(timeout)
-		var _options = JSON.parse(options)
-		var platform = 'ios'
-		_options.vigour.native.selectedPlatforms = platform
-		return build(_options)
-			.then(checkSuccess)
-	})
-	it("android should succeed in under " + timeout + " milliseconds!"
-	, function () {
-		this.timeout(timeout)
-		var _options = JSON.parse(options)
-		var platform = 'android'
-		_options.vigour.native.selectedPlatforms = platform
-		return build(_options)
-			.then(checkSuccess)
-	})
-	
+  it("ios should succeed in under " + timeout +  " milliseconds!"
+  , function () {
+    this.timeout(timeout)
+    var _options = JSON.parse(options)
+    var platform = 'ios'
+    _options.vigour.native.selectedPlatforms = platform
+    return build(_options)
+      .then(checkSuccess)
+  })
+  it('android should succeed in under ' + timeout + ' milliseconds!'
+  , function () {
+    this.timeout(timeout)
+    var _options = JSON.parse(options)
+    var platform = 'android'
+    _options.vigour.native.selectedPlatforms = platform
+    return build(_options)
+      .then(checkSuccess)
+  })
+
 })
 
 function checkSuccess (success) {
-	expect(success).to.equal(true)
+  expect(success).to.equal(true)
 }
