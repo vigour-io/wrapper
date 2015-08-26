277e640e600165452ecaa2cfbc742e3816fa8d0a
Michiel van Liempt <michiel.v.liempt@gmail.com>
2015-08-26 16:15:38 +0200

    #29 extracted functions from build script

--- test/node/android/build/index.js
+++ test/node/android/build/index.js
@@ -8,28 +8,41 @@ var repo = path.join(__dirname
   , '..', '..', '..', '..', '..', 'vigour-example')
 var pkgPath = path.join(repo, 'package.json')
 var opts =
-  { configFiles: pkgPath,
-  vigour:
-    { native:
-      {
-        root: repo
-      }
+{ configFiles: pkgPath,
+  vigour: { native: {
+      root: repo
     }
   }
+}
 
 var options = JSON.stringify(opts)
 var timeout = 1200000
 
+/*
+TODO create these test:
+- options result in gradle arguments (appId, version(Code))
+- tree copy:
+  - whole tree
+  - don't overwrite
+  - newer files
+  - exclude stuff (build, .gradle, .idea)
+- the template stuff:
+  - reading file
+  - writing file
+  - inserting code at point
+  - commenting line
+- adding plugin leads to modifying build and java files
+*/
 describe('build', function () {
   it('android should succeed in under ' + timeout + ' milliseconds!'
-  , function () {
-    this.timeout(timeout)
-    var _options = JSON.parse(options)
-    var platform = 'android'
-    _options.vigour.native.selectedPlatforms = platform
-    return build(_options)
-      .then(checkSuccess)
-  })
+    , function () {
+      this.timeout(timeout)
+      var _options = JSON.parse(options)
+      var platform = 'android'
+      _options.vigour.native.selectedPlatforms = platform
+      return build(_options)
+        .then(checkSuccess)
+    })
 })
 
 function checkSuccess (success) {
