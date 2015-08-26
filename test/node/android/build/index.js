9100f8f2974ceb93ab778979e1631dddfe799c52
Michiel van Liempt <michiel.v.liempt@gmail.com>
2015-08-26 16:50:21 +0200

    added pending tests

--- test/node/android/build/index.js
+++ test/node/android/build/index.js
@@ -9,30 +9,44 @@ var repo = path.join(__dirname
 var pkgPath = path.join(repo, 'package.json')
 var opts =
 { configFiles: pkgPath,
-  vigour: { native: {
+  vigour: {
+    native: {
       root: repo
     }
   }
 }
 
 var options = JSON.stringify(opts)
-var timeout = 1200000
+var timeout = 60000
 
-/*
-TODO create these test:
-- options result in gradle arguments (appId, version(Code))
-- tree copy:
-  - whole tree
-  - don't overwrite
-  - newer files
-  - exclude stuff (build, .gradle, .idea)
-- the template stuff:
-  - reading file
-  - writing file
-  - inserting code at point
-  - commenting line
-- adding plugin leads to modifying build and java files
-*/
+describe('android-scripts', function () {
+  describe('installTemplate', function () {
+    it('should copy all files to empty dir')
+    it('should not copy build and Android Studio files')
+    it('should not copy older files')
+    it('should overwrite newer files')
+  })
+
+  describe('assemble', function () {
+    it('should call gradle with params for the relevant options')
+  })
+
+  describe('install & run', function () {
+    it('should only install & run if set in options')
+    it('should use correct options to install apk')
+    it('should use correct options to run app')
+  })
+
+  describe('installing plugins', function () {
+    it('should init all plugins in main java file')
+    it('should build all plugins to .aar')
+    it('should add all .aar files to libs')
+    it('should add all .aar libs as dependency')
+    it('should add all plugin permissions to the manifest')
+    it('should work without any plugins')
+  })
+})
+
 describe('build', function () {
   it('android should succeed in under ' + timeout + ' milliseconds!'
     , function () {
