92b28786666bf70fb574f936cbb8e0d5d113f49a
Shawn Inder <shawn@vigour.io>
2015-11-23 12:15:30 -0500

    Log when native side receives a message or sends one

--- templates/ios/vigour-native/vigour-native/Bridge/VigourBridge.swift
+++ templates/ios/vigour-native/vigour-native/Bridge/VigourBridge.swift
@@ -19,53 +19,53 @@ enum VigourBridgeError: ErrorType {
 }
 
 class VigourBridge: NSObject, WKScriptMessageHandler {
-    
+
     var pluginManager:VigourPluginManager = {
         #if DEBUG
             print("*****CREATE PLUGIN MANAGER*****")
         #endif
         return VigourPluginManager()
     }()
-    
+
     weak var delegate: VigourViewController? {
         didSet {
 
         }
     }
-    
+
     override init() {
         super.init()
         setup()
     }
-    
+
     class func scriptMessageHandlerName() -> String {
         return scriptMessageHandlerString
     }
-    
-    
+
+
     private func setup() {
        //additional setup
     }
 
     final func activate() {
-        
+
         var token: dispatch_once_t = 0
         dispatch_once(&token) { [weak self] () -> Void in
             self?.makePluginsAvailable()
         }
-        
+
         //TODO: call generic ready
         //
     }
-    
+
     private final func makePluginsAvailable() {
         VigourPluginManager.pluginTypeMap.forEach{ (pluginId, type) in
             //VigourBridgeSendMessage.Ready
             if let plug = VigourPluginManager.pluginTypeMap[pluginId] {
-                
+
                 //get an insance or shared instance
                 let p = plug.instance()
-                
+
                 //call
                 do {
                     try sendJSMessage(VigourBridgeSendMessage.Ready(error: nil, response: p.onReady(), pluginId: pluginId))
@@ -74,13 +74,15 @@ class VigourBridge: NSObject, WKScriptMessageHandler {
                     //TODO: throw it to js side!
                 }
                 catch let error as NSError {
-                    
+
                 }
             }
         }
     }
-    
+
     internal final func sendJSMessage(message: VigourBridgeSendMessage) {
+        print("Sending")
+        print(message)
         if let d = delegate, webView = d.webView {
             webView.evaluateJavaScript(message.jsString(), completionHandler: { (_, error) -> Void in
                 if error != nil {
@@ -91,16 +93,17 @@ class VigourBridge: NSObject, WKScriptMessageHandler {
     }
 
     internal final func receiveBridgeMessage(message:VigourBridgeReceiveMessage) {
-
+        print("Receiving")
+        print(message)
         if let plug = VigourPluginManager.pluginTypeMap[message.pluginId] {
-            
+
             //get an insance or shared instance
             let p = plug.instance()
 
             //call the method
             do {
                 try p.callMethodWithName(message.pluginMethod, andArguments: message.arguments, completionHandler: { [weak self] (error, result) -> Void in
-                    
+
                     if error != nil {
                         print(error)
                         self?.sendJSMessage(VigourBridgeSendMessage.Error(error: error, pluginId: message.pluginId))
@@ -116,35 +119,35 @@ class VigourBridge: NSObject, WKScriptMessageHandler {
             catch let error as NSError {
                 print(error.localizedDescription)
             }
-            
+
         }
-        
+
     }
-    
+
     private final func processScriptMessage(message:WKScriptMessage) throws -> VigourBridgeReceiveMessage? {
         if let messageObject = message.body as? NSDictionary where messageObject.count >= 3 {
-        
+
             guard (messageObject.objectForKey("pluginId") as? String != nil) else { throw VigourBridgeError.BridgeError("Plugin id required!") }
-            
+
             guard (messageObject.objectForKey("fnName") as? String != nil) else { throw VigourBridgeError.BridgeError("Plugin id required!") }
-            
+
             if let pluginId = messageObject.objectForKey("pluginId") as? String,
                 let fnName = messageObject.objectForKey("fnName") as? String {
-                    
+
                 return VigourBridgeReceiveMessage(callbackId: messageObject.objectForKey("cbId") as? Int, pluginId:pluginId, pluginMethod: fnName, arguments:messageObject.objectForKey("opts") as? NSDictionary)
-                    
+
             }
         }
         return nil
     }
-    
+
     //MARK: - WKScriptMessageHandler
-    
+
     func userContentController(userContentController: WKUserContentController, didReceiveScriptMessage message: WKScriptMessage) {
-        
+
         if let messageObject = message.body as? NSDictionary where messageObject.count >= 2
             && message.name == self.dynamicType.scriptMessageHandlerName() {
-            
+
             do {
                 if let bridgeMessage = try processScriptMessage(message) {
                     receiveBridgeMessage(bridgeMessage)
@@ -156,9 +159,9 @@ class VigourBridge: NSObject, WKScriptMessageHandler {
             catch let error as NSError {
                 print(error.localizedDescription)
             }
-            
+
         }
-        
+
     }
-    
+
 }
