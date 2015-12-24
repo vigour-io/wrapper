87a3aa21760e1a7dacb6eff579ce100707d0b9b2
Alexander van der Werff <avdwerff@gmail.com>
2015-12-24 16:43:34 +0100

    connected product id's to apple backend fetch

--- templates/ios/vigour-native/vigour-native/Plugin/Purchase.swift
+++ templates/ios/vigour-native/vigour-native/Plugin/Purchase.swift
@@ -9,11 +9,15 @@
 import Foundation
 import StoreKit
 
+enum PurchaseMethod: String {
+    case Init="init", GetProducts="getProducts", Buy="buy"
+}
+
 public class Purchase:NSObject, SKProductsRequestDelegate, SKPaymentTransactionObserver, VigourPluginProtocol {
     
     static let sharedInstance = Purchase()
     
-//    private let productIdentifiers: Set<String>
+    private var productIdentifiers: Set<String> = ([])
 //    private var purchasedProductIdentifiers = Set<String>()
     
 //    private var productsRequest: SKProductsRequest?
@@ -21,16 +25,30 @@ public class Purchase:NSObject, SKProductsRequestDelegate, SKPaymentTransactionO
     
     override init() {
         super.init()
+        
+        //prepare
+        setup()
     }
     
     //MARK:- VigourPluginProtocol
     
-    static let pluginId:String = "purchase"
+    static let pluginId:String = "pay"
     
     weak var delegate: VigourBridgeViewController?
     
     func callMethodWithName(name: String, andArguments args: NSDictionary?, completionHandler: pluginResult) throws {
-        print(name)
+        guard let method = PurchaseMethod.init(rawValue: name)
+        else {
+            throw VigourBridgeError.PluginError("Unsupported method!", pluginId: Orientation.pluginId)
+        }
+        switch method {
+        case .Init:
+            break
+        case .GetProducts:
+            getProducts(completionHandler)
+        case .Buy:
+            break
+        }
     }
     
     static func instance() -> VigourPluginProtocol {
@@ -46,12 +64,41 @@ public class Purchase:NSObject, SKProductsRequestDelegate, SKPaymentTransactionO
     public func productsRequest(request: SKProductsRequest, didReceiveResponse response: SKProductsResponse) {
         if response.products.count != 0 {
             for product in response.products {
-                print(product)
+                print(product.price)
             }
         }
         else {
             print("There are no products.")
+            
         }
+    }
+    
+    //MARK:- Prepare
+    
+    private func setup() {
+        
+        //check for product id's in package
+        if let path = NSBundle.mainBundle().pathForResource("www/package", ofType:"json") {
+            do {
+                let jsonData = try NSData(contentsOfFile: path, options: NSDataReadingOptions.DataReadingMappedIfSafe)
+                let json = try NSJSONSerialization.JSONObjectWithData(jsonData, options: .AllowFragments)
+                
+                if let vigour = json["vigour"] as? [String: AnyObject],
+                    let pay = vigour["pay"] as? [String: AnyObject],
+                    let iOS = pay["iOS"] as? [String: AnyObject],
+                    let products = iOS["products"] as? [String: AnyObject] {
+                        for product in products {
+                            productIdentifiers.insert(product.1 as! String)
+                        }
+                }
+                
+            }
+            catch {
+                print("error serializing JSON: \(error)")
+            }
+        }
+        
+        
     }
     
     //MARK:- SKPaymentTransactionObserver
@@ -63,16 +110,16 @@ public class Purchase:NSObject, SKProductsRequestDelegate, SKPaymentTransactionO
     //MARK:- Plugin API
     
     
-    public func getProducts() {
+    private func getProducts(completionHandler: pluginResult) {
         if SKPaymentQueue.canMakePayments() {
-            let productIdentifiers = NSSet(array: productIDs)
-            let productRequest = SKProductsRequest(productIdentifiers: productIdentifiers as! Set<NSObject>)
+//            let productIdentifiers = Set(["mtv_play_single_episode_purchase", "mtv_play_subscription_annual", "mtv_play_subscription_monthly"])
+            let productRequest = SKProductsRequest(productIdentifiers: productIdentifiers)
             
             productRequest.delegate = self
             productRequest.start()
         }
         else {
-            println("Cannot perform In App Purchases.")
+            completionHandler(JSError(title: "Pay error", description: "Can't make payments", todo: "Check if account is setup with bank info"), JSValue([:]))
         }
     }
     
