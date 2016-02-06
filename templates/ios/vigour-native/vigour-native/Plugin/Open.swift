da1e3a591b47f832ba21f66a713f787d956e50e5
Alexander van der Werff <avdwerff@gmail.com>
2016-02-07 00:17:29 +0100

    new plugin open

--- templates/ios/vigour-native/vigour-native/Plugin/Open.swift
+++ templates/ios/vigour-native/vigour-native/Plugin/Open.swift
@@ -40,27 +40,15 @@ class Open: NSObject, VigourPluginProtocol {
         if let method = VigourOpenMethod(rawValue: name) {
             switch method {
             case .Init:
-                completionHandler(nil, JSValue([
-                    "name": "outside"
-                  ]
-                  )
-                )
-
+                completionHandler(nil, JSValue(["name": "outside"]))
             case .Open:
 
-                if let link = args?.objectForKey("url") as? String {
-
-                    let url = OpenURLValue(
-                        urlString: link
-                    )
+                if let urlString = args?.objectForKey("url") as? String, let url = NSURL(string: urlString) {
                     open(url, completionHandler:completionHandler)
-
                 }
                 else {
                     completionHandler(JSError(title: "Open needs a link", description: "No link found to open", todo: "`open.url.val = <url>`?"), JSValue([:]))
                 }
-
-
             }
         }
         else {
@@ -70,74 +58,18 @@ class Open: NSObject, VigourPluginProtocol {
     }
 
     func onReady() throws -> JSValue {
-
         //init stuff
-
         return JSValue([Open.pluginId:"ready"])
     }
 
     //Methods
 
-    private func login(scope:[String], completionHandler: PluginResult) {
-
-        let loginMgr = FBSDKLoginManager()
-
-        //The view controller to present from. If nil, the topmost view controller will be automatically determined as best as possible.
-        loginMgr.logInWithReadPermissions(scope, fromViewController: nil, handler: { (result, error) -> Void in
-            if error != nil {
-                completionHandler(JSError(title: "Open plugin error: \(error.code)", description: error.localizedDescription, todo: error.localizedRecoverySuggestion), JSValue([:]))
-                #if DEBUG
-                    print("FB ERROR: \(error.localizedDescription)")
-                #endif
-                return
-            }
-            else if result != nil {
-                var repsonse:[String:NSObject] = [
-                    "isCancelled":result.isCancelled
-                ]
-                if result.grantedPermissions != nil {
-                    repsonse["grantedPermissions"] = Array(result.grantedPermissions)
-                }
-                if result.declinedPermissions != nil {
-                    repsonse["declinedPermissions"] = Array(result.declinedPermissions)
-                }
-                if result.token != nil {
-                    repsonse["connectionStatus"] = "connected"
-                    repsonse["token"] = result.token.tokenString != nil ? result.token.tokenString : ""
-                    repsonse["userID"] = result.token.userID != nil ? result.token.userID : ""
-                    repsonse["userId"] = result.token.expirationDate != nil ? result.token.expirationDate.description : ""
-                }
-                completionHandler(nil, JSValue(repsonse))
-            }
-        })
-    }
-
-    private func logout(completionHandler: PluginResult) {
-        let loginMgr = FBSDKLoginManager()
-        loginMgr.logOut()
-        FBSDKAccessToken.setCurrentAccessToken(nil)
-        completionHandler(nil, JSValue([:]))
-    }
-
-    private func open(url: OpenURLValue, completionHandler: PluginResult) {
-        shareCompletionHandler = completionHandler
+    private func open(url: NSURL, completionHandler: PluginResult) {
         #if DEBUG
-            print("SHARING:: ", shareValue)
+            print("open url:: ", url)
         #endif
-        let content = FBSDKShareLinkContent()
-        content.contentTitle = shareValue.title
-        if let description = shareValue.description {
-            content.contentDescription = description
-        }
-        if let url = NSURL(string: shareValue.urlString) {
-            content.contentURL = url
-        }
-        if let imageUrlString = shareValue.imageUrlString, let imageUrl = NSURL(string: imageUrlString) {
-            content.imageURL = imageUrl
-        }
-        if let d = delegate as? UIViewController {
-            FBSDKShareDialog.showFromViewController(d, withContent: content, delegate: self)
-        }
+        UIApplication.sharedApplication().openURL(url)
+        completionHandler(nil, JSValue(true))
     }
 
 }
