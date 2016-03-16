d0e095257329bf40c30b6c64c854c0615df343f4
Renan Carvalho <renansscarvalho@gmail.com>
2016-03-16 14:48:01 +0100

    refactoring tests

--- test/node/webostv/builder/cleanup.js
+++ test/node/webostv/builder/cleanup.js
@@ -6,33 +6,17 @@ var sinon = require('sinon')
 var removeStub = sinon.stub()
 var path = require('path')
 var cleanUp
+var buildDir = path.join(__dirname + '../../../../app/build/lgtv/webos/')
 
-test('Should call cleanUp just once',function (assert) {
+test('Should call cleanUp',function (assert) {
    cleanUp = proxyquire('../../../../lib/builder/cleanup', {
     'vigour-fs-promised': { removeAsync: removeStub }
   })
 
-  cleanUp.apply({"buildDir":"teste"})
+  cleanUp.apply({"buildDir":buildDir})
+  assert.plan(2)
 
   assert.equal(removeStub.callCount, 1)
+  assert.equal(removeStub.firstCall.args[0],buildDir)
   assert.end()
-})
-
-test('Should clean the folder',function (assert) {
-  var folder
-  var fs = require('fs')
-  var buildDir = path.join(__dirname + '../../../../app/build/lgtv/webos/')
-  cleanUp = require('../../../../lib/builder/cleanup')
-
-  cleanUp.apply({"buildDir":buildDir})
-
-  try {
-    folder = fs.lstatSync(buildDir)
-    if(folder.isDirectory()) {
-    }
-  }
-  catch (e) {
-    assert.equal(e.code, 'ENOENT')
-    assert.end()
-  }
 })
--- test/node/webostv/builder/createstructure.js
+++ test/node/webostv/builder/createstructure.js
@@ -7,32 +7,15 @@ var createStub = sinon.stub()
 var path = require('path')
 var createStructure
 
-test('Should call createStructure just once',function (assert) {
+test('Should call createStructure',function (assert) {
   var buildDir = path.join(__dirname + '../../../../app/build/lgtv/webos/')
   createStructure = proxyquire('../../../../lib/builder/webostv/createStructure', {
     'vigour-fs-promised': { mkdirp: createStub }
   })
-
   createStructure.apply({"buildDir":buildDir})
 
+  assert.plan(2)
   assert.equal(createStub.callCount, 1)
+  assert.equal(createStub.firstCall.args[0],buildDir)
   assert.end()
-
 })
-
-// // test('Should clean the folder',function (assert) {
-// //   var folder
-// //   var fs = require('fs')
-// //   var buildDir = path.join(__dirname + '../../../../app/build/lgtv/webos/')
-// //   cleanUp = require('../../../../lib/builder/cleanup')
-
-// //   cleanUp.apply({"buildDir":buildDir})
-
-// //   try {
-// //     fs.lstatSync(buildDir).isDirectory()
-// //   }
-// //   catch (e) {
-// //     assert.equal(e.code, 'ENOENT')
-// //     assert.end()
-// //   }
-// // })
