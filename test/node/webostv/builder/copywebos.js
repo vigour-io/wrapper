b66d0d1d6bbefd690f6a64744bb0b3cd8b4e581d
Renan Carvalho <renansscarvalho@gmail.com>
2016-03-17 12:05:30 +0100

    webos copy tests

--- test/node/webostv/builder/copywebos.js
+++ test/node/webostv/builder/copywebos.js
@@ -4,7 +4,7 @@ var test = require('tape')
 var proxyquire = require('proxyquire')
 var sinon = require('sinon')
 var path = require('path')
-var templateDir = path.join(__dirname + '/../../../../templates/webostv/')
+var templateDir = path.join(__dirname + '/../../../../templates/webostv')
 var buildDir = path.join(__dirname + '../../../../app/build/lgtv/webos/')
 var copyWebOs
 var fs = require('vigour-fs/lib/server')
@@ -22,8 +22,16 @@ test('Copying webostv file', (assert) => {
   copyWebOs = proxyquire('../../../../lib/builder/webostv/copywebos', {
     'vigour-fs/lib/server': resolveStub
   })
-  assert.plan(1)
+
+  assert.plan(3)
   copyWebOs.apply(fakeData)
-  assert.equal(resolveStub.callCount,1,'should copy the webos file')
+  assert.equal(resolveStub.callCount,1, 'should copy the webos file')
+  assert.equal(resolveStub.firstCall.args[0], templateDir+'/webos.js' , 'should pass the correct param')
+
+  resolveStub.reset()
+  fakeData.builds = false
+  copyWebOs.apply(fakeData)
+  assert.equal(resolveStub.callCount,0, 'should not copy the webos file')
+
   assert.end()
 })
